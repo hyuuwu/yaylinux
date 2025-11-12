4e8066050a5a2970ecda7c7928ff67884dfa3436
Luiz <61273547+hyuuwu@users.noreply.github.com>
2025-11-11 21:08:51 -0300

    Implement first run check and sound alerts
    Added first run check and sound notifications.

--- main.py
+++ main.py
@@ -5,13 +5,31 @@ from pytube import YouTube
 import random as uwu
 import install
 import subprocess as sub
+import sys
+import platform
+from playsound import playsound
+
+chime = "chime.mp3"
+signoff = "signoff.mp3"
 
 def run(app):
     sub.run([app])
+CONFIG_FILE = "dontdeletethis.file"
+
+def is_first_run():
+    if not os.path.exists(CONFIG_FILE):
+        # This is the first run
+        with open(CONFIG_FILE, "w") as f:
+            f.write("hi,this is a file to check if the user ran this file. ever. if you want to delete it,its yo choice,but you gon redo the setup")
+        return True
+    else:
+        # Not the first run
+        return False
 
 
-open('password', 'w').close()
-open('hostname', 'w').close()
+
+
+
 
 def apt():
      smthsmth = input("wut app u want")
@@ -63,8 +81,8 @@ def cd(new_path):
 
 
 
-
-print("IBH (c)2010-2024 \nAll rights reserved ;)\n256 mb RAM : ok \n1.66ghz CPU: ok \n1000mb HDD : ok")
+playsound(chime, True)
+print("IBH (c)2010-2025 \nAll rights reserved ;)\n256 mb RAM : ok \n1.66ghz CPU: ok \n1000mb HDD : ok")
 print(".")
 time.sleep(2)
 print(".")
@@ -74,37 +92,52 @@ print(".")
 input("press key.enter to contiinueeeeeee")
 
 print("welcome to YAY pyth-linux.\nY = yolo\nA= autism\nY = yeet")
+if is_first_run():
+    open('user', 'w').close()
+    open('hostname', 'w').close()
+    ihateicks = input("Hello user,we will kindly ask you to say your username \nInsert here: ")
+    with open("user", "a") as f:
+        f.write(ihateicks)
 
-ihateicks = input("Hello user,we will kindly ask you to say your temp username \nInsert here: ")
-
-print("HEYYYY", ihateicks, "WE MISSED YOU SO MUCH")
-kk = input("want to have an passw? Yes(y) No(n)")
-if kk == "y":
-      ikendrick = input("insert pass")
-      with open("password", "a") as f:
-          f.write(ikendrick)
-
-if kk == "y":
-    heyhelp = input("insert pass")
-    if heyhelp == ikendrick:
+    print("HEYYYY", ihateicks, "WE MISSED YOU SO MUCH")
+    ikendrick = input("create an pass")
+    with open("password", "a") as f:
+        f.write(ikendrick)
+        heyhelp = input("insert pass")
+        if heyhelp == ikendrick:
              print("preciate it")
+        else:
+            print("wrong pass,but ok")
+            print(f"if you forget,the pass is {ikendrick}.")
+
+    hostnameeee = input("what hostname you want to use \n")
+    with open("hostname", "a") as f:
+        f.write(hostnameeee)
+else:
+    with open("hostname", "r") as h_file:
+        hostnameeee = h_file.read().strip()
+
+
+    with open("user", "r") as u_file:
+        ihateicks = u_file.read().strip()
+
+    with open("password", "r") as p_file:
+        verify = p_file.read().strip()
+
+    verifypass = input(f"Hello {ihateicks}, Welcome back to YAYLinux! whats your password?\n")
+    if verifypass == verify:
+        print("Welcome back!")
     else:
-         print("YOU SON OF A BITCH")
-         while True:
-              wb.open("hyuuwu.tech")
-
-hostnameeee = input("what hostname you want to use \n")
-with open("hostname", "a") as f:
-    f.write(hostnameeee)
+        print("Wrong password, try again at reboot")
+        sys.exit("wrong pass >:(")
 
 
-print("now we really starting")
 
 while True:
      terminal = input(f"{ihateicks}@{hostnameeee} $ ~/")
      
      if terminal == "help":
-      print("we aint helpin- jk. LIST OF COMMANDS:\nhelp: help.\nkanye: kanye song\nspaghetto: sekreti\nexit: exit\nmkdir: makes an dir\ncd: cd\nls: ls\nvim: rip ur pc\nytb: YOUTUBE DOWNLOADER YAHOO!\nopnytb: opens an url,but we want u to open yt tho\nhydra: opens its github page\nlauncher:wip\ncalc:wip")
+      print("we aint helpin- jk. LIST OF COMMANDS:\nhelp: help.\nkanye: kanye song\nspaghetto: sekreti\nexit: exit\ncd: cd\nls: ls\nvim: rip ur pc\nytb: YOUTUBE DOWNLOADER YAHOO!\nopnytb: opens an url,but we want u to open yt tho\nhydra: opens its github page\nlauncher:wip\ncalc:wip")
       if terminal == "kanye":
           wb.open("https:/ /youtube.com/watch?v=wuO4_P_8p-Q&si=1j47jLi9GMSNRFsI")
           print("fire")
@@ -114,18 +147,12 @@ while True:
           print("orochinho")
           print("hidden command lmaoooooooooooooooooooo")
      if terminal == "exit":
-          print("end")
-          wb.open("hyuaae.site/yaylinux")
+          print("Thanks For Using!")
+          playsound(signoff, True)
           break
-     if terminal == "mkdir":
-          dir_name = input("nam3")
-          dir_path = input("pat")  # Let user provide a path
-    
-          try:
-               os.mkdir(os.path.join(dir_path, dir_name))  # Combine path and name safely
-               print("mad3!")
-          except OSError as error:
-               print("sowwy :( :", error)
+
+
+
 
      if terminal == "ls":  # Use a common 'ls' command for listing
           list_files()
@@ -133,7 +160,7 @@ while True:
      if terminal == "cd":
           j = input("wut dir?: ")
           cd(j)
-#this shit crashed python
+          #this shit crashed python
      if terminal == "vim":
           print("how to crash ur pc")
           owo = uwu.randint(1,5)
@@ -167,10 +194,10 @@ while True:
              install.install(efgh)
 
      if terminal == "calc":
-          firstn = input("first number")
-          secondn = input("second number")
-          print(firstn + secondn)
-          print("thanks 4 using")
+         firstn = input("first number")
+         secondn = input("second number")
+         print(int(firstn) + int(secondn))
+         print("thanks 4 using")
 
 
 
@@ -180,4 +207,11 @@ while True:
 
           
      else:
-         run(terminal)
+         if platform.system == "Linux":
+             klmn = sub.run(["sh", "-c", terminal])
+         else:
+            j = sub.run(["powershell", "-Command", terminal])
+            if j.returncode == 0:
+                print("app run")
+            else:
+                print("oops")

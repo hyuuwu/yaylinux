52389e04e9404ee15ec9ad60a40a87f4382e6fc6
Luiz <61273547+hyuuwu@users.noreply.github.com>
2025-11-17 11:26:23 -0300

    Refactor install function to use platform module

--- install.py
+++ install.py
@@ -1,9 +1,10 @@
 import subprocess as sub
 import os
 # import time
+import platform
 
 def install(app):
-    if os.name == "nt":
+    if platform.system() == 'Windows' :
         look4choco = sub.run(["where", "choco"])
         if look4choco.returncode == 0:
             installation = sub.run(["choco", "install", app, "-y"])
@@ -20,12 +21,12 @@ def install(app):
             else:
                 print("choco didnt install. quiting")
     else:
-        if os.uname == "Linux":
-            linux = sub.run(["apt", "install", app, "-y"], stdout=sub.PIPE)
+        if platform.system() == 'Linux':
+            linux = sub.run(["sudo", "-S", "pacman", "-S", app], stdout=sub.PIPE)
             if linux.returncode == 0:
                 print("app(or cli command,nerd) installed")
             else:
-                print("u prob use arch. btw,error")
+                print("u prob use debian. btw,error")
         else:
             if os.uname == "Darwin":
                 print("uhh idk how to install things on mac")
